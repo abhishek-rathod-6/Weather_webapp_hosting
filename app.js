d4129593d7dcd2e1bf2a68552ac466066f842c89
Abhi <abhishekrathod496@gmail.com>
2022-04-10 18:52:34 +0530

    Weather Webapp Hosting

---
+++ app.js
@@ -0,0 +1,31 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var weather    = require('openweather-apis');
+
+//init
+var app = express();
+
+//set the template engine
+app.set('view engine','ejs');
+
+//fetch the data from request
+app.use(bodyParser.urlencoded({extended:false}));
+
+//default pageload
+app.get('/',function(req,res){
+    res.render('home',{temp:null});
+});
+
+app.post('/',function(req,res){
+    weather.setCity(req.body.city);
+    weather.setAPPID('bf2cc86c585ee0b933a2d43a47c9bfe5');
+    weather.getAllWeather(function(err,temp){
+        console.log(temp);
+        res.render('home',{temp:temp});
+    });
+});
+
+var port  = process.env.PORT || 5000;
+app.listen(port,function(){
+    console.log('server running at '+port);
+});
